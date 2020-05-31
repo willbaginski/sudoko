574ca85c86a99989a1736521e97b28b3707c84a5
Lily S. Maechling 23 <lmaechling@moose.cs.dartmouth.edu>
2020-05-31 18:23:48 -0400

    mutilpe solutions solver

--- common/sudokustruct.h
+++ common/sudokustruct.h
@@ -15,7 +15,7 @@ bool sudoku_load(sudoku_t *sudoku);
 
 bool sudoku_build(sudoku_t *sudoku);
 
-int sudoku_solve(sudoku_t *sudoku, int solution);
+int sudoku_solve(sudoku_t *sudoku);
 
 bool sudoku_print(sudoku_t *sudoku);
 
